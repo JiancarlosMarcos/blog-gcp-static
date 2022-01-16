ac2740a9276e2a49506e0e557fb74980009d69b2
jiancarlos <jiancarlosmarcosp@gmail.com>
2022-01-15 19:16:25 -0500

    responsive corregido y buton expanded

--- server.js
+++ server.js
@@ -1,26 +1,10 @@
 const express = require('express');
 var app = express();
 
-// app.get('/asd',(req, res)=>{
-//   res.send('holitas del mar asd');
-// });
 const PORT = process.env.PORT || 8080;
+
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
 });
-// app.set('porti',4000);
+
 app.use(express.static('public'));
-
-// app.listen(app.get('port'),()=>{
-//   console.log('servidor ',app.get('port'));
-// });
-
-// app.get('/',(req,res)=>res.render('prod/page/indes.html'));
-// app.get('/:handle',(req,res)=>{
-//   res.render('${req.params.handle}')
-// })
-
-
-// app.listen(app.get('port'),function(){
-//   console.log('servidor escuchando %d' ,app-get('port'))
-// })
